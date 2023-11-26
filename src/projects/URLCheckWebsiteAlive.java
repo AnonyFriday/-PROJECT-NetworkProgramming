82d5acfc3cec684214d6d7687e394323015d75a9
AnonyFriday <duyvukim752000@gmail.com>
2023-11-26 13:42:11 +0700

    Using HttpURLConnection to test the responseCode and check the website's existence

--- src/projects/URLCheckWebsiteAlive.java
+++ src/projects/URLCheckWebsiteAlive.java
@@ -4,6 +4,14 @@
  */
 package projects;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author duyvu
@@ -13,6 +21,35 @@ public class URLCheckWebsiteAlive {
     public static void main(String[] args) {
         String[] websites = {
             "https://titv.vn",
-            "https://vnexpress.net",};
+            "https://vnexpress1.net",
+            "https://google.com"
+        };
+
+        for (String website : websites) {
+            checkWebsite(website);
+        }
+    }
+
+    public static void checkWebsite(String urlString) {
+        try {
+            URL url = URI.create(urlString).toURL();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            /**
+             * Http Code
+             * 200: OK
+             * 401: Unauthorized
+             * 403: Forbidden
+             * 500: Internal Server Error
+             * 404: Not Found
+             */
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+                System.out.println("This website is active");
+            }
+
+        } catch (Exception es) {
+            System.out.println(urlString + " Cannot connect!");
+        }
     }
 }
