705bcdc73026d1097fd358fc4cafc2d1cd69d18b
AnonyFriday <duyvukim752000@gmail.com>
2023-11-26 13:05:57 +0700

    Adding URLCheckWebsiteAlive file

---
+++ src/projects/URLCheckWebsiteAlive.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projects;
+
+/**
+ *
+ * @author duyvu
+ */
+public class URLCheckWebsiteAlive {
+
+    public static void main(String[] args) {
+        String[] websites = {
+            "https://titv.vn",
+            "https://vnexpress.net",};
+    }
+}
