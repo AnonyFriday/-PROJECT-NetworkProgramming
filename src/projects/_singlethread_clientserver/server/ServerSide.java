f2ddfe13a11dfa0860a1866a6476281bd7513d53
AnonyFriday <duyvukim752000@gmail.com>
2023-11-27 15:48:22 +0700

    Create the QuoteService as the hashmap containing key,value of the product's price Create the ServerSide (not thread yet) receive and return the price based on the prompt from client

---
+++ src/projects/_singlethread_clientserver/server/ServerSide.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projects._singlethread_clientserver.server;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author duyvu
+ */
+public class ServerSide {
+    // ============================
+    // = Fields
+    // ============================
+
+    public final static int SERVER_PORT = 9999;
+    public final static String SERVER_IP = "localhost";
+    private final static QuoteService quoteService = new QuoteService();
+
+    // ============================
+    // = Main function for sending request and get response from 1 client
+    // ============================
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket()) {
+            serverSocket.bind(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+
+            while (true) {
+                System.out.println("Waiting for client ...");
+                // Listen for the connection to this socket and accept it 
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client accepted: " + clientSocket);
+
+                // Access the input and output stream of the client socket
+                InputStream inputStream = clientSocket.getInputStream();
+                OutputStream outputStream = clientSocket.getOutputStream();
+
+                // Receive the message from the client
+                BufferedInputStream bis = new BufferedInputStream(inputStream);
+                byte[] request = new byte[100];
+
+                if (bis.read(request) != -1) {
+                    // Getting product from the client
+                    String product = new String(request).trim();
+                    String price = quoteService.getQuote(product);
+
+                    // Check if price is not null
+                    if (price == null) {
+                        price = "\t+ The product info is invalid";
+                    }
+
+                    // Send back the price to the client 
+                    outputStream.write(price.getBytes());
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(projects._nothread_clientserver.ServerSide.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
